2f7b55e15ae5c765ad86216b0769fe02c6f8990f
Shaunna <skwiens@gmail.com>
2018-01-03 14:28:44 -0800

    user and record model setup

--- main.py
+++ main.py
@@ -6,6 +6,32 @@ app = Flask(__name__)
 app.config.from_object(DevConfig)
 db = SQLAlchemy(app)
 
+class User(db.Model):
+    id = db.Column(db.Integer(), primary_key=True)
+    email = db.Column(db.String(255), unique=True, nullable=False)
+    password = db.Column(db.String(255), nullable=False)
+    records = db.relationship('Record', backref='user', lazy='dynamic')
+
+    def __init__(self, username):
+        self.username = username
+
+    def __repr__(self):
+        return "<User '{}'>".format(self.username)
+
+class Record(db.Model):
+    id = db.Column(db.Integer(), primary_key=True)
+    date = db.Column(db.DateTime(), unique=True)
+    volunteers = db.Column(db.String(255))
+    notes = db.Column(db.Text())
+    shopping_list = db.Column(db.Text())
+    user_id = db.Column(db.Integer(), db.ForeignKey('user.id'))
+
+    def __init__(self, date):
+        self.date = date
+
+    def __repr__(self):
+        return "<Record '{}'>".format(self.title)
+
 @app.route('/')
 def index():
     # return '<h1>hello world</h1>'
