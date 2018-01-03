0b64abcb976289abb3ab04973eae366d78a6e925
Shaunna <skwiens@gmail.com>
2018-01-03 11:18:27 -0800

    connect to database

--- main.py
+++ main.py
@@ -1,8 +1,10 @@
 from flask import Flask, render_template
 from config import DevConfig
+from flask.ext.sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
 app.config.from_object(DevConfig)
+db = SQLAlchemy(app)
 
 @app.route('/')
 def index():
