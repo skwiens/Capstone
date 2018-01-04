868cfb27c729eeefb0828850c3f35358f390f554
Shaunna <skwiens@gmail.com>
2018-01-04 10:20:57 -0800

    registration functionality

--- app/main.py
+++ app/main.py
@@ -1,6 +1,11 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request, url_for, redirect, session, flash
 from config import DevConfig
 from flask_sqlalchemy import SQLAlchemy
+# from flask_wtf import FlaskForm
+from wtforms import Form, StringField, TextAreaField, PasswordField, validators
+from wtforms.validators import InputRequired, Email, Length
+from passlib.hash import sha256_crypt
+from functools import wraps
 
 app = Flask(__name__)
 app.config.from_object(DevConfig)
@@ -9,12 +14,17 @@ db = SQLAlchemy(app)
 
 class User(db.Model):
     id = db.Column(db.Integer(), primary_key=True)
+    name = db.Column(db.String(100))
+    username = db.Column(db.String(50), unique=True)
     email = db.Column(db.String(255), unique=True, nullable=False)
     password = db.Column(db.String(255), nullable=False)
     records = db.relationship('Record', backref='user', lazy='dynamic')
 
-    def __init__(self, username):
+    def __init__(self, name, username, email, password):
+        self.name = name
         self.username = username
+        self. email = email
+        self.password = password
 
     def __repr__(self):
         return "<User '{}'>".format(self.username)
@@ -35,8 +45,37 @@ class Record(db.Model):
 
 @app.route('/')
 def index():
-    # return '<h1>hello world</h1>'
     return render_template('index.html')
+
+class RegisterForm(Form):
+    name = StringField('Name', [validators.Length(min=1, max=50)])
+    username = StringField('Username', [validators.Length(min=4, max=25)])
+    email = StringField('Email', [validators.DataRequired(), validators.Email(), validators.Length(min=6, max=50)])
+    password = PasswordField('Password', [
+        validators.DataRequired(),
+        validators.EqualTo('confirm', message='Passwords do not match')
+    ])
+    confirm = PasswordField('Confirm Password')
+
+@app.route('/register', methods=['GET', 'POST'])
+def register():
+    form = RegisterForm(request.form)
+    if request.method == 'POST' and form.validate():
+        new_user = User(
+            name = form.name.data,
+            username = form.username.data,
+            email = form.email.data,
+            password = sha256_crypt.encrypt(str(form.password.data))
+        )
+
+        db.session.add(new_user)
+        db.session.commit()
+
+        flash('You are now registered and can log in', 'success')
+
+        return redirect(url_for('index'))
+    return render_template('register.html', form=form)
+
 
 if __name__ == '__main__':
     app.run()
