d4a10746035e2e30a6d1d12cd21a758d55122b86
Shaunna <skwiens@gmail.com>
2018-01-09 21:30:36 -0800

    change to production config

--- app/__init__.py
+++ app/__init__.py
@@ -4,7 +4,7 @@ from .config import app_config
 
 app = Flask(__name__, instance_relative_config=True)
 
-app.config.from_object(app_config['dev'])
+app.config.from_object(app_config['prod'])
 app.config.from_pyfile('config.py')
 
 db = SQLAlchemy(app)
