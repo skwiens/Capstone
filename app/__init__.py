58b75af7e93d50098b4ae9043ab80db0f763d4aa
Shaunna <skwiens@gmail.com>
2018-01-10 16:33:16 -0800

    config file for deployment

--- app/__init__.py
+++ app/__init__.py
@@ -1,13 +1,12 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
-from .config import app_config
+from .config import AppConfig
 from flask_login import LoginManager
 
 app = Flask(__name__, instance_relative_config=True)
 
-app.config.from_object(app_config['dev'])
-
-app.config.from_pyfile('config.py')
+app.config.from_object(AppConfig)
+# app.config.from_pyfile('config.py')
 
 db = SQLAlchemy(app)
 
