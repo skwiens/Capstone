964b237793c6d038671138a2043698a5088690d4
Shaunna <skwiens@gmail.com>
2018-01-13 18:13:11 -0800

    now sending emails through send_email, but errors out if user admin not signed in

--- app/views.py
+++ app/views.py
@@ -8,21 +8,60 @@ from flask_login import LoginManager, login_required, current_user, login_user, 
 from requests_oauthlib import OAuth2Session
 from .config import Auth
 
+# FOR GMAIL API SENDING EMAILS:
+##########################################
+import httplib2
+import os
+import oauth2client
+from oauth2client import client, tools
+import base64
+from email.mime.multipart import MIMEMultipart
+from email.mime.text import MIMEText
+from apiclient import errors, discovery
+import mimetypes
+from email.mime.image import MIMEImage
+from email.mime.audio import MIMEAudio
+from email.mime.base import MIMEBase
+#################################
+
 import os
 import google.oauth2.credentials
 import google_auth_oauthlib.flow
 import googleapiclient.discovery
+import sendgrid
+# from sendgrid.helpers.mail import *
 
 
 #### HELP!  NEED THIS TO BE NOT A FILE! #####
 CLIENT_SECRETS_FILE = 'client_secret.json'
+CLIENT_SECRET_FILE = 'client_secret.json'
 # CLIENT_SECRETS_FILE = os.environ['CLIENT_SECRETS_FILE']
 SCOPES = ['https://www.googleapis.com/auth/gmail.compose']
 API_SERVICE_NAME = 'gmail'
 API_VERSION = 'v1'
+APPLICATION_NAME = 'Bethany Food Bank'
 
 import os
 os.environ['OAUTHLIB_INSECURE_TRANSPORT'] = '1'
+
+
+
+def get_credentials():
+    home_dir = os.path.expanduser('~')
+    credential_dir = os.path.join(home_dir, '.credentials')
+    if not os.path.exists(credential_dir):
+        os.makedirs(credential_dir)
+    credential_path = os.path.join(credential_dir, 'gmail-python-email-send.json')
+    store = oauth2client.file.Storage(credential_path)
+    credentials = store.get()
+    if not credentials or credentials.invalid:
+        flow = client.flow_from_clientsecrets(CLIENT_SECRET_FILE, SCOPES)
+        flow.user_agent = APPLICATION_NAME
+        credentials = tools.run_flow(flow, store)
+        print('Storing credentials to ' + credential_path)
+    return credentials
+
+
 
 def user_logged_in(f):
     @wraps(f)
@@ -101,7 +140,6 @@ def oauth2callback():
 
     credentials = flow.credentials
     session['credentials'] = credentials_to_dict(credentials)
-
 
     return redirect(url_for('admin_login'))
 
@@ -379,10 +417,122 @@ def note(id):
 
     return render_template('note.html', note=note, openhour=openhour)
 
-# @app.route('/send_email')
-# def send_message():
-#     main()
+def CreateMessageHtml(sender, to, subject, msgHtml, msgPlain):
+    msg = MIMEMultipart('alternative')
+    msg['Subject'] = subject
+    msg['From'] = sender
+    msg['To'] = to
+    msg.attach(MIMEText(msgPlain, 'plain'))
+    msg.attach(MIMEText(msgHtml, 'html'))
+    # return {'raw': base64.urlsafe_b64encode(msg.as_string())}
+    return {'raw': base64.urlsafe_b64encode(msg.as_string().encode()).decode('ascii')}
 
+def SendMessage(sender, to, subject, msgHtml, msgPlain, attachmentFile=None):
+    credentials = get_credentials()
+    http = credentials.authorize(httplib2.Http())
+    service = discovery.build('gmail', 'v1', http=http)
+    # if attachmentFile:
+    #     message1 = createMessageWithAttachment(sender, to, subject, msgHtml, msgPlain, attachmentFile)
+    # else:
+    #     message1 = CreateMessageHtml(sender, to, subject, msgHtml, msgPlain)
+    # result = SendMessageInternal(service, "me", message1)
+    if 'user' in session and session['user'] == 'admin':
+        message1 = CreateMessageHtml(sender, to, subject, msgHtml, msgPlain)
+        result = SendMessageInternal(service, "me", message1)
+        return result
+    else:
+        redirect(url_for('admin_login'))
+
+def SendMessageInternal(service, user_id, message):
+    try:
+        message = (service.users().messages().send(userId=user_id, body=message).execute())
+        print('Message Id: %s' % message['id'])
+        return message
+    except errors.HttpError as error:
+        print('An error occurred: %s' % error)
+        return "Error"
+    return "OK"
+
+def CreateMessageHtml(sender, to, subject, msgHtml, msgPlain):
+    msg = MIMEMultipart('alternative')
+    msg['Subject'] = subject
+    msg['From'] = sender
+    msg['To'] = to
+    msg.attach(MIMEText(msgPlain, 'plain'))
+    msg.attach(MIMEText(msgHtml, 'html'))
+    # return {'raw': base64.urlsafe_b64encode(msg.as_string())}
+    return {'raw': base64.urlsafe_b64encode(msg.as_string().encode()).decode('ascii')}
+
+def createMessageWithAttachment(
+    sender, to, subject, msgHtml, msgPlain, attachmentFile):
+    """Create a message for an email.
+
+    Args:
+      sender: Email address of the sender.
+      to: Email address of the receiver.
+      subject: The subject of the email message.
+      msgHtml: Html message to be sent
+      msgPlain: Alternative plain text message for older email clients
+      attachmentFile: The path to the file to be attached.
+
+    Returns:
+      An object containing a base64url encoded email object.
+    """
+    message = MIMEMultipart('mixed')
+    message['to'] = to
+    message['from'] = sender
+    message['subject'] = subject
+
+    messageA = MIMEMultipart('alternative')
+    messageR = MIMEMultipart('related')
+
+    messageR.attach(MIMEText(msgHtml, 'html'))
+    messageA.attach(MIMEText(msgPlain, 'plain'))
+    messageA.attach(messageR)
+
+    message.attach(messageA)
+
+    print("create_message_with_attachment: file: %s" % attachmentFile)
+    content_type, encoding = mimetypes.guess_type(attachmentFile)
+
+    if content_type is None or encoding is not None:
+        content_type = 'application/octet-stream'
+    main_type, sub_type = content_type.split('/', 1)
+    if main_type == 'text':
+        fp = open(attachmentFile, 'rb')
+        msg = MIMEText(fp.read(), _subtype=sub_type)
+        fp.close()
+    elif main_type == 'image':
+        fp = open(attachmentFile, 'rb')
+        msg = MIMEImage(fp.read(), _subtype=sub_type)
+        fp.close()
+    elif main_type == 'audio':
+        fp = open(attachmentFile, 'rb')
+        msg = MIMEAudio(fp.read(), _subtype=sub_type)
+        fp.close()
+    else:
+        fp = open(attachmentFile, 'rb')
+        msg = MIMEBase(main_type, sub_type)
+        msg.set_payload(fp.read())
+        fp.close()
+    filename = os.path.basename(attachmentFile)
+    msg.add_header('Content-Disposition', 'attachment', filename=filename)
+    message.attach(msg)
+
+    return {'raw': base64.urlsafe_b64encode(message.as_string())}
+
+@app.route('/send_email')
+def send_email():
+    to = "xana.wines.ada@gmail.com"
+    sender = "xana.wines.ada@gmail.com"
+    subject = "subject"
+    msgHtml = "Hi<br/>Html Email"
+    msgPlain = "Hi\nPlain Email"
+    SendMessage(sender, to, subject, msgHtml, msgPlain)
+    # Send message with attachment:
+    # SendMessage(sender, to, subject, msgHtml, msgPlain, '/path/to/file.pdf')
+    flash('Hahahahahaha', 'success')
+    return redirect(url_for('index'))
 
 if __name__ == '__main__':
     app.run(debug=True)
