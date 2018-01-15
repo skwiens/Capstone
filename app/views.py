50e8c94129d235ed3a2019ac9ee9b71434fd22da
Shaunna <skwiens@gmail.com>
2018-01-15 07:48:08 -0800

    error handling pages

--- app/views.py
+++ app/views.py
@@ -383,6 +383,15 @@ def note(id):
 # def send_message():
 #     main()
 
+########################
+@app.errorhandler(404)
+def not_found_error(error):
+    return render_template('404.html'), 404
+
+@app.errorhandler(500)
+def internal_error(error):
+    db.session.rollback()
+    return render_template('500.html'), 500
 
 if __name__ == '__main__':
     app.run(debug=True)
