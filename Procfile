b65458be463ce13fcd419a11c577bf1564329065
Shaunna <skwiens@gmail.com>
2018-01-17 14:34:07 -0800

    db for heroku

--- Procfile
+++ Procfile
@@ -1,1 +1,4 @@
 web: gunicorn app:app
+init: python app db init
+migrate: python app db migrate
+upgrade: python app db upgrade
--- app/__init__.py
+++ app/__init__.py
@@ -1,6 +1,5 @@
 #app/__init__.py
 
-
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from .config import AppConfig
